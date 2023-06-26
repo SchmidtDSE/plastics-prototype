5e9b9b869c0a207de84fc1d8a8f6c2414cd764a9
Sam Pottinger <sam.pottinger@berkeley.edu>
2023-06-26 13:27:33 -0700

    Small fix in load_deps.

--- sh/load_deps.sh
+++ sh/load_deps.sh
@@ -1,5 +1,6 @@
 [ ! -e test/qunit.css ] && wget https://cdnjs.cloudflare.com/ajax/libs/qunit/2.19.4/qunit.min.css -O test/qunit.css
 [ ! -e test/qunit.min.js ] && wget https://cdnjs.cloudflare.com/ajax/libs/qunit/2.19.4/qunit.min.js -O test/qunit.min.js
+mkdir -p third_party
 [ ! -e third_party/ace.min.js ] && wget https://cdnjs.cloudflare.com/ajax/libs/ace/1.23.0/ace.min.js -O third_party/ace.min.js
 [ ! -e third_party/theme-textmate.js ] && wget https://cdnjs.cloudflare.com/ajax/libs/ace/1.23.0/theme-textmate.min.js -O third_party/theme-textmate.js
 [ ! -e third_party/theme-textmate-css.js ] && wget https://cdnjs.cloudflare.com/ajax/libs/ace/1.23.0/theme-textmate-css.min.js -O third_party/theme-textmate-css.js
