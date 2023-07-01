1420267524f4f32353ef5d10ac9c0072b536d25a
Sam Pottinger <sam.pottinger@berkeley.edu>
2023-06-30 18:16:23 -0700

    Fix in package.sh

--- sh/package.sh
+++ sh/package.sh
@@ -1,7 +1,9 @@
 mkdir -p deploy
 cp -r css deploy/css
+cp -r data deploy/data
 cp -r font deploy/font
 cp -r js deploy/js
 cp -r pt deploy/pt
+cp -r template deploy/template
 cp -r third_party deploy/third_party
 cp index.html deploy
