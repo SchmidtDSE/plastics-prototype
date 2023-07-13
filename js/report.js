88340ab10deff4a51f7fe907591ec3d0485b4145
Sam Pottinger <sam.pottinger@berkeley.edu>
2023-07-13 15:28:41 -0700

    Separate out goals logic.

--- js/report.js
+++ js/report.js
@@ -1,5 +1,4 @@
 import {
-    ALL_REGIONS,
     DEFAULT_YEAR,
     DEFAULT_REGION,
     DISPLAY_TYPES,
@@ -7,6 +6,7 @@ import {
     CONSUMPTION_ATTRS,
     EOL_ATTRS,
 } from "const";
+import {getGoals} from "goals";
 import {BubblegraphPresenter} from "report_bubble";
 import {ConfigPresenter} from "report_config";
 import {GoalPresenter} from "report_goals";
@@ -382,40 +382,7 @@ class ReportPresenter {
 
     _getGoals(target, selection) {
         const self = this;
-
-        const strategies = [
-            (output) => {
-                const total = EOL_ATTRS.filter((x) => x !== "eolRecyclingMT")
-                    .map((x) => output.get(x))
-                    .reduce((a, b) => a + b);
-
-                return {
-                    "goal": "nonRecycledWaste",
-                    "value": total,
-                };
-            },
-            (output) => {
-                return {
-                    "goal": "mismanagedWaste",
-                    "value": output.get("eolMismanagedMT"),
-                };
-            }
-        ];
-
-        const goals = new Map();
-        ALL_REGIONS.forEach((region) => {
-            const regionOutputs = target.get("out").get(region);
-            const newValues = strategies.map((x) => x(regionOutputs));
-            
-            const regionGoals = new Map();
-            newValues.forEach((newValue) => {
-                regionGoals.set(newValue["goal"], newValue["value"]);
-            });
-
-            goals.set(region, regionGoals);
-        });
-
-        return goals;
+        return getGoals(target);
     }
 }
 
