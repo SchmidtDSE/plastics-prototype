26b3ab950517c87560ba5d3f262f84b40f1ef619
A Samuel Pottinger <sam@gleap.org>
2023-09-15 15:23:07 -0700

    Add move data.

---
+++ move_data.sh
@@ -0,0 +1,4 @@
+mv data/Pipeline\ Result/overview_ml.csv data/overview_ml.csv
+mv data/Pipeline\ Result/overview_curve.csv data/overview_curve.csv
+mv data/Pipeline\ Result/overview_naive.csv data/overview_naive.csv
+cp data/overview_ml.csv data/web.csv
