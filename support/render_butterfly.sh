82c736bf29075c74bb3dcbf7d005c846b121568a
Sam Pottinger <sam.pottinger@berkeley.edu>
2023-09-28 09:48:20 -0700

    Fix render_butterfly.sh

--- support/render_butterfly.sh
+++ support/render_butterfly.sh
@@ -1,3 +1,3 @@
-export ROOT_DIR = $PWD;
+export ROOT_DIR = '$PWD';
 cd image_gen
 $ROOT_DIR/processing-4.3/processing-java --sketch=butterfly --output=/tmp/butteflybuild --force --run $ROOT_DIR/data/Pipeline\ Result/combined.db $ROOT_DIR/img
