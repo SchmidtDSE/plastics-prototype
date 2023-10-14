91b98613a7dbec5d7aa2bb348ba74a178bd2dfa2
A Samuel Pottinger <sam@gleap.org>
2023-10-14 17:03:19 +0000

    Have pipeline getter check output.

--- support/get_pipeline_output.sh
+++ support/get_pipeline_output.sh
@@ -1,5 +1,5 @@
 mkdir /tmp/pipeline_staging
 wget --user plastics --password kSno3GeK https://global-plastics-tool.org/datapipeline.zip -O /tmp/pipeline_staging/pipeline.zip
 unzip /tmp/pipeline_staging/pipeline.zip -d /tmp/pipeline_staging
-mv /tmp/pipeline_staging/output /tmp/pipeline_staging/pipeline
-cp -r /tmp/pipeline_staging/pipeline pipeline
+mv /tmp/pipeline_staging/output pipeline
+[ ! -e pipeline/combined.db ] && exit 1;
