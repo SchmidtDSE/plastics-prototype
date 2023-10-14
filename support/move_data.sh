b07a7a9b5e4f11d2c87a393a0c6b492970896e41
A Samuel Pottinger <sam@gleap.org>
2023-10-14 16:59:38 +0000

    Use the published artifact for pipeline.

--- support/move_data.sh
+++ support/move_data.sh
@@ -1,4 +1,4 @@
-mv data/Pipeline\ Result/overview_ml.csv data/overview_ml.csv
-mv data/Pipeline\ Result/overview_curve.csv data/overview_curve.csv
-mv data/Pipeline\ Result/overview_naive.csv data/overview_naive.csv
+mv pipeline/overview_ml.csv data/overview_ml.csv
+mv pipeline/overview_curve.csv data/overview_curve.csv
+mv pipeline/overview_naive.csv data/overview_naive.csv
 cp data/overview_ml.csv data/web.csv
