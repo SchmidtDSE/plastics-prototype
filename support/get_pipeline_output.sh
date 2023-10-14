9607aa43f3916129282bbd8895f5a5b076382657
A Samuel Pottinger <sam@gleap.org>
2023-10-14 17:17:52 +0000

    Debug github action.

--- support/get_pipeline_output.sh
+++ support/get_pipeline_output.sh
@@ -1,5 +1,7 @@
 mkdir /tmp/pipeline_staging
 wget --user plastics --password kSno3GeK https://global-plastics-tool.org/datapipeline.zip -O /tmp/pipeline_staging/pipeline.zip
 unzip /tmp/pipeline_staging/pipeline.zip -d /tmp/pipeline_staging
+[ ! -e /tmp/pipeline_staging/output/combined.db ] && exit 1;
 mv /tmp/pipeline_staging/output pipeline
-[ ! -e pipeline/combined.db ] && exit 1;
+ls pipeline
+[ ! -e pipeline/combined.db ] && exit 2;
