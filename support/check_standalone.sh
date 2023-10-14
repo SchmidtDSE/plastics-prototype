237b4e02b90690db788654829a6a129f8db3d357
A Samuel Pottinger <sam@gleap.org>
2023-10-14 23:46:17 +0000

    Update check standalone.

--- support/check_standalone.sh
+++ support/check_standalone.sh
@@ -1,5 +1,4 @@
-[ ! -e standalone_tasks/all.json ] && exit 1;
-[ ! -e standalone_tasks/highAmbition.json ] && exit 2;
-[ ! -e standalone_tasks/lowAmbition.json ] && exit 3;
+[ ! -e standalone_tasks/highAmbition.json ] && exit 1;
+[ ! -e standalone_tasks/lowAmbition.json ] && exit 2;
 echo "Test files found."
 exit 0
