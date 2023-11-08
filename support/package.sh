d1e8f911c413ebd5783eb36c48bfffd4a78a98a9
A Samuel Pottinger <sam@gleap.org>
2023-11-08 19:37:32 +0000

    Humans updates.

--- support/package.sh
+++ support/package.sh
@@ -12,5 +12,8 @@ cp index.html deploy
 cp preview.html deploy
 cp robots.txt deploy
 cp humans.txt deploy
+
+python support/update_humans.py deploy/humans.txt
+
 epoch=$(date +%s)
 sed -i "s/EPOCH/${epoch}/g" deploy/index.html
