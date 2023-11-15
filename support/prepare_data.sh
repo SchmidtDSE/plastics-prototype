e5ed19014ffaaeabb27b1b4b5908c870cc95be73
A Samuel Pottinger <sam@gleap.org>
2023-11-15 16:35:50 +0000

    Correction in prepare data.

--- support/prepare_data.sh
+++ support/prepare_data.sh
@@ -10,11 +10,13 @@ mv pipeline/overview_ml.csv data/overview_ml_original.csv
 mv pipeline/overview_curve.csv data/overview_curve_original.csv
 mv pipeline/overview_naive.csv data/overview_naive_original.csv
 mv pipeline/combined.db data/combined.db
-cp data/overview_ml.csv data/web.csv
 
 echo "== Splitting primary / secondary =="
 python support/separate_production.py data/overview_ml_original.csv data/overview_ml.csv 1 20 2011
 python support/separate_production.py data/overview_curve_original.csv data/overview_curve.csv 1 20 2011
 python support/separate_production.py data/overview_naive_original.csv data/overview_naive.csv 1 20 2011
+
+echo "== Make primary web output =="
+cp data/overview_ml.csv data/web.csv
 
 echo "== Data prepared =="
