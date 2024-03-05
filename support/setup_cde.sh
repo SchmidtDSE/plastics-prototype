7919dc7fe6235474e38071dfad13704752d9e2e4
A Samuel Pottinger <sam@gleap.org>
2024-03-05 00:30:52 +0000

    Integrate render index.

--- support/setup_cde.sh
+++ support/setup_cde.sh
@@ -1,3 +1,7 @@
+pip install -r requirements.txt
+bash support/render_index.sh
+[ ! -e pt/index.json ] && exit 1;
+
 bash support/setup_local.sh
 bash support/install_processing.sh
 bash support/render_line_graphs.sh
--- support/setup_local.sh
+++ support/setup_local.sh
@@ -9,5 +9,3 @@ bash support/make.sh
 bash support/update_scenarios_default.sh
 bash support/run_scenarios_standalone.sh
 bash support/render_template.sh
-
-pip install -r requirements.txt
