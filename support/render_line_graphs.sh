ef9d1f704c7596f9a9a1843917f3c4994f4939a8
A Samuel Pottinger <sam@gleap.org>
2024-10-11 19:27:12 +0000

    Add SVG for line graph.

--- support/render_line_graphs.sh
+++ support/render_line_graphs.sh
@@ -1,2 +1,3 @@
 python image_gen/line.py data/combined.db img/line.png regions.json
 python image_gen/line.py data/combined.db img/line.pdf regions.json
+python image_gen/line.py data/combined.db img/line.svg regions.json
